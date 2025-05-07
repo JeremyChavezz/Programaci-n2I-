c238f13c9ee4badfdb83778051c231caabe6b5da
Jeremy <Jeremyjeremychavezz55@gmail.com>
2025-05-07 09:53:02 -0500

    proyecto2

---
+++ proyecto3/proyecto3.py
@@ -0,0 +1,6 @@
+#proyecto 3 Tabla de multiplicar personalizada 
+x= int(input(Ingrese un número ))
+print(fTabla de multiplicar del {x} )
+for i in range(1,13)
+    producto=ix
+    print(producto, producto)
